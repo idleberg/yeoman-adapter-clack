970d603fcf9c30f9625e8d8a5fcf018c1dc2a2dc
Jan T. Sott <git@idleberg.com>
2026-01-28 21:20:26 +0100

    chore: update comment

--- src/adapter-compat.ts
+++ src/adapter-compat.ts
@@ -22,7 +22,7 @@ import type { PromptAnswers, PromptQuestions } from 'yeoman-generator';
  * - input: Maps to clack.text()
  * - password: Maps to clack.password()
  * - confirm: Maps to clack.confirm()
- * - list/rawlist: Maps to clack.select()
+ * - list/rawlist/select: Maps to clack.select()
  * - checkbox: Maps to clack.multiselect()
  * - expand: Maps to clack.select() with key hints in message
  * - number: Maps to clack.text() with number validation
